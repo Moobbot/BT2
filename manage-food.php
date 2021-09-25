d8aae2e84797b9544aded9309dd482dfe741187e
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-09-25 21:58:04 +0700

    Khoi-tao

---
+++ manage-food.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/icon/all.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <ul class="nav justify-content-center fw-bold border-bottom border-3 border-secondary fs-5">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Adimin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Category</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Food</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Order</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Logout</a>
+            </li>
+        </ul>
+    </header>
+
+    <section class=" bg-gray-200">
+        <div class="container">
+            <div class="row mb-5 pt-5">
+                <h2 class="fw-bold">Manage Category</h2>
+            </div>
+
+            <div class="row-cols-6 mb-4">
+                <button type="button" class="btn btn-primary">Add Category</button>
+            </div>
+
+            <div class="row">
+                <div class="row">
+                    <div class="col-1 fw-bold m-1 border-bottom border-2 border-dark">S.N.</div>
+                    <div class="col-2 fw-bold m-1 border-bottom border-2 border-dark">Title</div>
+                    <div class="col-2 fw-bold m-1 border-bottom border-2 border-dark">Image</div>
+                    <div class="col-1 fw-bold m-1 border-bottom border-2 border-dark">Featured</div>
+                    <div class="col-1 fw-bold m-1 border-bottom border-2 border-dark">Active</div>
+                    <div class="col fw-bold m-1 border-bottom border-2 border-dark">Actions</div>
+                </div>
+                <div class="row">
+                    <div class="row m-3">
+                        <div class="col-1 mt-auto mb-auto">1.</div>
+
+                        <div class="col-2 mt-auto mb-auto">
+                            <p>Pizza</p>
+                        </div>
+
+                        <div class="col-2">
+                            <img src="assets/img/pizza.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-success bg-light-success text-dark fw-bold">
+                                Update Category
+                            </button>
+
+                        </div>
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-outline-hotpink fw-bold">
+                                Delete Category
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row m-3">
+                        <div class="col-1 mt-auto mb-auto">2.</div>
+
+                        <div class="col-2 mt-auto mb-auto">
+                            <p>Burger</p>
+                        </div>
+
+                        <div class="col-2">
+                            <img src="assets/img/burger.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-success bg-light-success text-dark fw-bold">
+                                Update Category
+                            </button>
+
+                        </div>
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-outline-hotpink fw-bold">
+                                Delete Category
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row m-3">
+                        <div class="col-1 mt-auto mb-auto">3.</div>
+
+                        <div class="col-2 mt-auto mb-auto">
+                            <p>Momo</p>
+                        </div>
+
+                        <div class="col-2">
+                            <img src="assets/img/momo.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-success bg-light-success text-dark fw-bold">
+                                Update Category
+                            </button>
+
+                        </div>
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-outline-hotpink fw-bold">
+                                Delete Category
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row m-3">
+                        <div class="col-1 mt-auto mb-auto">4.</div>
+
+                        <div class="col-2 mt-auto mb-auto">
+                            <p>Quia est ipsum id id</p>
+                        </div>
+
+                        <div class="col-2">
+                            <img src="assets/img/bg.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>No</p>
+                        </div>
+
+                        <div class="col-1 mt-auto mb-auto">
+                            <p>Yes</p>
+                        </div>
+
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-success bg-light-success text-dark fw-bold">
+                                Update Category
+                            </button>
+
+                        </div>
+                        <div class="col mt-auto mb-auto ">
+                            <button class="btn btn-outline-hotpink fw-bold">
+                                Delete Category
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="row bg-hotpink text-center justify-content-center">
+            <p>
+                2020 All right rerved. Food House. Developed By.
+                <a href="https://vietcodedi.com/login/index.php">CSE485</a>
+            </p>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
